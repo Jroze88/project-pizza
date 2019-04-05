04d11edc8019d2e663c0e93b04931aca16c12209
Kevin Augustin <kevinaugustin91@gmail.com>
2019-04-05 13:38:48 -0400

    troubleshooting scraper

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
-//Initializing Packages
-const express = require('express')//Express is server manager
-const path = require('path')//Path is middleware that connects directories
-const PORT = process.env.PORT || 3001;//Port is just a number, no magic. like saying var port = 3001;
+const cheerio = require('cheerio')
+const $ = cheerio.load('pfp.html')
 
-const app = express()//Turning key to activate connection
-//app methods include : app.get app.post and app.listen
-//Initialized Packages
+var eventTitle = [];
 
-//Define middleware here - these two format URLs and JSON objects respectively
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
-//Defined middleware
+$('.list.items .item').each(function(index, element){
+	eventTitle[index] = {};
+	var eventTitle = $(element).find('eds-media-card-content__title');
+});
 
-//wtf does our server even do?
-//homepage
-app.get('/', function(req, res){
-app.use(express.static( path.join(__dirname, '/index.html')))
-})
-
-app.listen(PORT, function() {
-console.log('is this thing on')
-}) //app.listen(needs two arguments) => (url,action)
+console.log(eventTitle); // Output the data in the terminal
